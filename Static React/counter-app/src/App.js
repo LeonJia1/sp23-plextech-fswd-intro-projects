8136e602e19c8d4dd041df2d4aed14d8f4e5847e
Leon <leonjia0924@gmail.com>
2023-03-11 20:53:58 -0800

    buttons

--- Static React/counter-app/src/App.js
+++ Static React/counter-app/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button.js';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  function increment() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>Count: {count}</a>
+        <a>
+          <Button title = "-" task = {() => decrement()}/>
+          <Button title = "+" task = {() => increment()}/>
         </a>
+        
       </header>
     </div>
   );
